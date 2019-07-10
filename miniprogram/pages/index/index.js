afc369dbf36f23ac84c26e8ff550ce3a50a52bae
unknown <unknown@example.com>
2019-07-10 16:17:48 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,38 @@
+
+var app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+ 
+    currentData:0
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //获取当前滑块的index
+ 
+  },
+
+  
+  
+
+  num()
+  {
+  
+    let n = app.globalData.currentData;
+    this.setData({
+     
+      currentData: n
+    })
+
+  }
+
+ 
+
+
+
+})
